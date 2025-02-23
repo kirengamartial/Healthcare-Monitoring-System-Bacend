1634ae4c4d7d6ac4c44995e4c000f9a38a2c6b46
kirengamartial <kirengamartial@gmail.com>
2025-02-23 16:02:39 +0200

    configure cors

--- src/app.js
+++ src/app.js
@@ -6,17 +6,10 @@ require('dotenv').config();
 
 const app = express();
 
-// CORS configuration
 const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://healthmonitorsystem.netlify.app'
+  origin: ['*','http://localhost:5173','https://healthmonitorsystem.netlify.app'
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Allow credentials (cookies, authorization headers, etc)
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  maxAge: 3600 // Maximum age (in seconds) of the preflight request cache
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
